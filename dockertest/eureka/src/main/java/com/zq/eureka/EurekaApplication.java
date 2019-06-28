c1bebb4f4c2fad758aa69c37b696cb3647620c71
zhaiqi-seed <52315339+zhaiqi-seed@users.noreply.github.com>
2019-06-28 17:06:48 +0800

    Add files via upload

---
+++ dockertest/eureka/src/main/java/com/zq/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.zq.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zq
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
